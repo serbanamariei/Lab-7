f153195a7180d641b864761ac1a5321595f24707
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 18:53:10 +0300

    Create functii.h

---
+++ problema-1.1/functii.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+int *pcitireVector(int n);
+void pafisareVectorIntregi(int *a, int n);
+void pafisareVectorReali(double *a, int n);
+double *vectorMedie(int *a, int *b, int n);
+double medieAritmeticaVector(double *a, int n);
+double mediaAritmeticaIntregi(int a, int b);
+double sumaVectorReali(double *a, int n);
+
+#endif
