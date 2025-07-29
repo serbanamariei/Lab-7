9e557873c3a8c5a4163ab979017662fe544bb6a4
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 18:56:18 +0300

    Create functii.h

---
+++ problema-1.3/functii.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+int *citireVectorInt(int n);
+void afisVectorInt(int *a, int n);
+int sumaCifre(int a);
+int *wvector(int *v, int n);
+int maximVector(int *v, int n);
+
+#endif
