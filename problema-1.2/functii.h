c1aaef928a0c6488aba8937edebf280dbc627bf5
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 18:54:42 +0300

    Create functii.h

---
+++ problema-1.2/functii.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+void pcitireVectorInt(unsigned int *a, int n);
+void pafisareVectorInt(unsigned int *a, int n);
+double *vectorRata(unsigned int *a,unsigned int c, int n);
+void pafisareVectorDouble(double *a, int n);
+double medieGeo(double *a, int n);
+double rataMedie(double *a, int n);
+
+#endif
